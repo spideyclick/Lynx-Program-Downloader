6b88261467d9f8ec2ce3f70515160dac774df50a
spideyclick <spideyclick@gmail.com>
2015-07-11 09:53:59 -0600

    Version 0.1:  The script downloads and sorts files specified in support txt's.

--- lpd.sh
+++ lpd.sh
@@ -16,15 +16,16 @@ WORKINGDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ###CONFIGURATION
 DOWNLOAD_DIRECTORY="$WORKINGDIR/downloads"
-LOGFILE="$WORKINGDIR/support/lpd.log"
+mkdir $DOWNLOAD_DIRECTORY > /dev/null 2>&1
+LOGFILE="$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/download_progress.log"
 touch $LOGFILE
-CONFIG_FILE="$WORKINGDIR/support/Programs for monthly update.csv"
 
 printlog () {
   echo $1
-  echo $1 >> $LOGFILE
   if [ $2 == "failed" ] ; then
-    echo $1 >> "$WORKINGDIR/logs/failed.txt"
+    echo "FAIL:  $1" >> $LOGFILE
+  else
+    echo $1 >> $LOGFILE
   fi
   }
 
@@ -39,7 +40,6 @@ for ARG ; do
     if [ "$ARG" == "-h" ] || [ "$ARG" == "--help" ] ; then
       echo "Usage: pdu.sh -i [USER'S INITIALS]... -s [DOWNLOAD SET]... -c -h"
       echo "  -h, --help 			prints this help message"
-      echo "  -i, --initials 		Specifies initials to be appended to file names"
       echo "  -s, --set 			allows you to choose from a predefined set of downloads"
       echo "  -c, --configure 		walks you through the configuration process"
       echo "  -r, --reset 			resets all logs"
@@ -52,16 +52,6 @@ for ARG ; do
       read DOWNLOAD_DIRECTORY
       UNKNOWN_OPT="0"
       shift
-    fi
-    if [ "$ARG" == "-i" ] || [ "$ARG" == "--initials" ] ; then
-      if [ -z "$2" ] ; then
-	echo "Please specify initials to be placed on downloads" && return 1
-      else
-	DOWNLOADER="$2"
-	echo "$DOWNLOADER will be appended to filenames."
-	UNKNOWN_OPT="0"
-	SKIP_ARG="1"
-      fi
     fi
     if [ "$ARG" == "-s" ] || [ "$ARG" == "--set" ] ; then
       if [ -z "$2" ] ; then
@@ -109,31 +99,36 @@ if [ "$?" == "0" ] ; then
   printlog "Package Manager: $PKGMAN"
 fi
 if [ -z "$PKGMAN" ] ; then
-  echo "Package manager not recognized!  Please make sure rpm or apt are installed and working!" && return 1
+  printlog "Package manager not recognized!  Please make sure rpm or apt are installed and working!" && return 1
 fi
+
 depcheck () {
   which "$1" >> /dev/null
   if [ "$?" != "0" ] ; then
-    echo "This program requires $1 to be installed in order to run properly.  You can install it by typing:"
+    printlog "This program requires $1 to be installed in order to run properly.  You can install it by typing:"
     if [ "$PKGMAN" == "apt" ] ; then
-      echo "sudo apt-get install $1"
+      printlog "sudo apt-get install $1"
+      INSACTN="apt-get"
     elif [ "$PKGMAN" == "rpm" ] ; then
-      echo #rpm apt-get command here
-    else echo "Package manager not recognized!  Please make sure rpm or apt are installed and working!" && return 1
+      printlog "yum install $1"
+      INSACTN="yum"
+      
+    else printlog "Package manager not recognized!  Please make sure rpm or apt are installed and working!" && return 1
     fi
-    echo "Or we can try to install it right now.  Would you like to? (Y/N)"
+    printlog "Or we can try to install it right now.  Would you like to? (Y/N)"
     UINPUT=0
     read UINPUT # grab first letter of input, upper or lower it, and check for THAT input.  Shorter.
     until [ $UINPUT == "exit" ] ; do
       if [ $UINPUT == "Y" ] || [ $UINPUT == "y" ] || [ $UINPUT == "yes" ] || [ $UINPUT == "Yes" ] || [ $UINPUT == "YES" ] ; then
-	sudo apt-get install lynx
+        printlog "Installing $1..."
+        sudo $INSACTN install $1
 	UINPUT="exit"
       elif [ $UINPUT == "N" ] || [ $UINPUT == "n" ] || [ $UINPUT == "no" ] || [ $UINPUT == "No" ] || [ $UINPUT == "NO" ] ; then
-	echo "Package install cancelled." && return 0
+	    printlog "Package install cancelled." && return 0
       else echo "I beg your pardon?"
       fi
     done
-  else echo "Dependency check of $1 success"
+  else printlog "Dependency check of $1 success"
   fi }
 
 progdownload () {
@@ -141,24 +136,26 @@ progdownload () {
   NOW=$(date +"%Y_%m_%d") && printlog "$DOWNLOAD_SET started at $NOW"
   MYNUM="0"
   until [ "$URL" == "exit" ] ; do
+    echo "" >> $LOGFILE
     MYNUM=$((MYNUM + 1))
-    URL="$(sed ''$MYNUM'q;d' $1)" && echo "$MYNUM) downloading $URL"
+    URL="$(sed ''$MYNUM'q;d' $1)" && printlog "$MYNUM) downloading $URL"
     mkdir "$WORKINGDIR/tmp" > /dev/null 2>&1
-    cd "$WORKINGDIR/tmp" && pwd
-    lynx -cmd_script="$WORKINGDIR/support/mgcmd.txt" --accept-all-cookies $URL && echo "lynx complete!"
-    FILE=`(ls | head -n 1)` && echo $FILE
-    EXT=`echo -n $FILE | tail -c 3` && echo $EXT
-    BAD=`cat "$WORKINGDIR/support/whiteexts.txt" | grep -v "#" | grep -cim1 "$EXT"` && echo $BAD
+    cd "$WORKINGDIR/tmp"
+    lynx -cmd_script="$WORKINGDIR/support/mgcmd.txt" --accept-all-cookies $URL
+    FILE=`(ls | head -n 1)`
     if [ -z "$FILE" ] ; then
       printlog "Download incomplete: $URL" "failed"
     else
+      EXT=`echo -n $FILE | tail -c 3`
+      BAD=`cat "$WORKINGDIR/support/whiteexts.txt" | grep -v "#" | grep -cim1 "$EXT"`
       until [ -z "$FILE" ] ; do
         if [ $BAD == "0" ] ; then
-            printlog "Download $FILE is of unknown type. $URL failed"
-            mv "$FILE" "$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/badfiles/$FILE"
-        else echo "new file downloaded."
+          mv "$FILE" "$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/badfiles/$FILE"
+          printlog "Download $FILE is of unknown type. $URL" "failed"
+        else
+          mv "$FILE" "$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/$DOWNLOAD_SET/$FILE"
+          printlog "Download success of $FILE from $URL"
         fi
-        mv "$FILE" "$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/$DOWNLOAD_SET/$FILE"
         FILE=`(ls | head -n 1)`
       done
     fi
@@ -168,21 +165,18 @@ progdownload () {
 
 ###PROGRAM START
 cd $WORKINGDIR
+echo "" >> $LOGFILE
+printlog "lpd started at `date`"
 depcheck wget
 depcheck lynx
 
 mkdir "$DOWNLOAD_DIRECTORY/`date +%Y-%m`" > /dev/null 2>&1
 mkdir "$DOWNLOAD_DIRECTORY/$DOWNLOAD_DATE/badfiles" > /dev/null 2>&1
 
-if [ -z "$DOWNLOADER" ] ; then
-  echo "Please enter your initials:"
-  read DOWNLOADER
-fi
-
 until [ "$DOWNLOAD_SET" == "exit" ] ; do
   UNKNOWN_OPT="1"
   echo "Which batch would you like to download?"
-  echo "all majorgeeks antivirus creative utilities office clear_logs configure exit"
+  echo "all antivirus creative utilities office clear_logs configure exit"
 #   DOWNLOAD_SELECTION="All Majorgeeks Wgets Antivirus Creative Utilities Office Clear_logs Configure Exit"
 #   select opt in $DOWNLOAD_SELECTION; do
 #     DOWNLOAD_SET="$opt"
@@ -190,16 +184,8 @@ until [ "$DOWNLOAD_SET" == "exit" ] ; do
   read DOWNLOAD_SET
   if [ "$DOWNLOAD_SET" == "all" ] ; then
     UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
+    printlog "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/wgetadrs.txt" "unsorted"
-    progdownload "$WORKINGDIR/support/antivirus.txt" "antivirus"
-    progdownload "$WORKINGDIR/support/creative.txt" "creative"
-    progdownload "$WORKINGDIR/support/utilities.txt" "utilities"
-    progdownload "$WORKINGDIR/support/office.txt" "office"
-  fi
-  if [ "$DOWNLOAD_SET" == "majorgeeks" ]; then
-    UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/support/antivirus.txt" "antivirus"
     progdownload "$WORKINGDIR/support/creative.txt" "creative"
     progdownload "$WORKINGDIR/support/utilities.txt" "utilities"
@@ -207,35 +193,35 @@ until [ "$DOWNLOAD_SET" == "exit" ] ; do
   fi
   if [ "$DOWNLOAD_SET" == "antivirus" ]; then
     UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
+    printlog "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/support/antivirus.txt" "antivirus"
   fi
   if [ "$DOWNLOAD_SET" == "creative" ]; then
     UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
+    printlog "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/support/creative.txt" "creative"
   fi
   if [ "$DOWNLOAD_SET" == "utilities" ]; then
     UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
+    printlog "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/support/utilities.txt" "utilities"
   fi
   if [ "$DOWNLOAD_SET" == "office" ]; then
     UNKNOWN_OPT="0"
-    echo "Downloading $DOWNLOAD_SET ..."
+    printlog "Downloading $DOWNLOAD_SET ..."
     progdownload "$WORKINGDIR/support/office.txt" "office"
   fi
   if [ "$DOWNLOAD_SET" == "clear_logs" ]; then
     UNKNOWN_OPT="0"
     if [ -n != "`ls $WORKINGDIR/logs/badfiles/`" ] ; then
-      rm -f $WORKINGDIR/logs/badfiles/* && echo "files cleared"
-    else echo "no files to clear"
+      rm -f $WORKINGDIR/logs/badfiles/* && printlog "files cleared"
+    else printlog "no files to clear"
     fi
     if [ -f $WORKINGDIR/logs/failed.txt ] ; then
       NOW=`date`
       mv $WORKINGDIR/logs/failed.txt "$WORKINGDIR/logs/failed: $NOW.txt"
-      echo "failed.txt archived and cleared."
-    else echo "no logs to clear"
+      printlog "failed.txt archived and cleared."
+    else printlog "no logs to clear"
     fi
   fi
   if [ "$DOWNLOAD_SET" == "configure" ]; then
@@ -246,7 +232,7 @@ until [ "$DOWNLOAD_SET" == "exit" ] ; do
   fi
   if [ "$DOWNLOAD_SET" == "exit" ]; then
     UNKNOWN_OPT="0"
-    echo "Goodbye!"
+    printlog "Goodbye!"
     DOWNLOAD_SET="exit"
   fi
   if [ "$UNKNOWN_OPT" == "1" ] ; then
